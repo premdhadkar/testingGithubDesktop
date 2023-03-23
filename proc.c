545875af1169d4d4ab3dc6c8a7d3fcd4290f6a57
PREM DHADKAR <premdhadkar@users.noreply.github.com>
2023-03-24 01:37:13 +0530

    basic code

---
+++ proc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<proc.c>
+int main(int argc, char const *argv[])
+{
+    int a;
+    if(a = fork()){
+        printf("true value :%d",a);
+    }else{
+        printf("false value :%d",a);
+    }
+    scanf("%d",&a);
+    printf("%d",a+10);
+    return 0;
+}
+
+
